a6bb74f7b7cb4be380b31c1e8568d64df3512de8
HeitorOrtega <rm5578@fiap.com.br>
2024-10-27 12:27:32 -0400

    Arrumando e terminando o projeto

--- portfolio/src/components/Rodape/Rodape.tsx
+++ portfolio/src/components/Rodape/Rodape.tsx
@@ -1,6 +1,8 @@
 const Rodape = () => {
   return (
     <footer className="bg-gray-900 text-white py-6 text-center">
+      <p className="text-sm font-light">&copy; Heitor Ortega - 557825 Marcos Lourenco - 556496 Pedro Saraiva - 555160 Leonardo Bianchi - 558576 Robert Daniel - 555881</p>
+      <br/>
       <p className="text-sm font-light">&copy; 2024 Portfolio. Todos os direitos reservados.</p>
     </footer>
   );
