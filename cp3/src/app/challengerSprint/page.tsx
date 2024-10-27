77a95b3dc7bef99167d5eeca9c6b880f796e2d39
leonardobianchii <leo-bianchi@hotmail.com>
2024-10-27 01:37:19 -0300

    Deletando node_modules

--- cp3/src/app/challengerSprint/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function challengerSprint(){
-    return(
-        <div>
-            <h2>Lista dos desafios: </h2>
-        </div>
-    )
-}
--- cp3/src/app/globalSolution/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function GlobalSolution(){
-    return(
-        <div>
-            <h2>Informações sobre as avaliações integradoras(Global Solution):</h2>
-        </div>
-    )
-}
